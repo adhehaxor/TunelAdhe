92e639145fd3fc09c9061a54ca7c15a458c11a40
adhehaxor <39878379+adhehaxor@users.noreply.github.com>
2024-01-10 04:20:03 +0700

    Update cf.sh
    Signed-off-by: adhehaxor <39878379+adhehaxor@users.noreply.github.com>

--- file/cf.sh
+++ file/cf.sh
@@ -9,10 +9,10 @@ echo ""
 echo ""
 #sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
 read -rp "Input Domain Name. Example ( kontol12 ): " -e sub
-DOMAIN=serverpremium.biz.id
-SUB_DOMAIN=${sub}.serverpremium.biz.id
-CF_ID=vscobangst111@gmail.com
-CF_KEY=4a912a2d56a1b3837d48751da5334b74a4fb8
+DOMAIN=adhestore1.my.id
+SUB_DOMAIN=${sub}.adhestore1.my.id
+CF_ID=adhesquad9@gmail.com
+CF_KEY=8bc45d51c8702c7774e2b5f80149460b
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
